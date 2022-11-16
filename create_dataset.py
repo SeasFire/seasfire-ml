4f96930a3382daba8cc9aff367c8341aa878c45e
Dimitrios Michail <dimitrios.michail@gmail.com>
2022-11-16 10:01:08 +0200

    Added time index

--- create_dataset.py
+++ create_dataset.py
@@ -32,9 +32,9 @@ class DatasetBuilder:
         # one of gwis_ba, BurntArea, frpfire, co2fire, FCCI_BA, co2fire
         self.target_var = "gwis_ba"
 
-        self.time_train = (0, 874)    # 2001-2019
-        self.time_test  = (874,920)   # 2020
-        self.time_eval  = (920, 966)  # 2021
+        self.time_train = (0, 874)  # 2001-2019
+        self.time_test = (874, 920)  # 2020
+        self.time_eval = (920, 966)  # 2021
 
         self.sp_res = 0.25  # Spatial resolution
 
@@ -46,63 +46,99 @@ class DatasetBuilder:
         logger.info("Opening zarr file {}".format(self.cube_path))
         self.cube = xr.open_zarr(self.cube_path, consolidated=False)
         logger.info("Cube: {}".format(self.cube))
-        #print(self.cube.data_vars)
-
+        # print(self.cube.data_vars)
 
     def create_samples(self, min_lon, min_lat, max_lon, max_lat):
         logger.info("Creating samples")
         region = self.cube.sel(
             latitude=slice(max_lat, min_lat), longitude=slice(min_lon, max_lon)
-        )#.isel(time=35)
+        )  # .isel(time=35)
 
         print(region)
 
         pass
 
-    def create_sample(self, lon, lat, time, radius=2, past_weeks=46, time_aggregation='3M'):
-        logger.info("Creating samples")
-        #region = self.cube.sel(
-        #    latitude=lat, longitude=lon
-        #).isel(time=35)
+    def create_sample(self, lat, lon, time, radius=2, weeks=58, time_aggregation="3M"):
+        logger.info(
+            "Creating sample for lat={}, lon={}, time={}, radius={}, weeks={}, time_aggregation={}".format(
+                lat, lon, time, radius, weeks, time_aggregation
+            )
+        )
 
-        #print (self.cube.coords["latitude"].to_numpy())
-        #print (self.cube.coords["longitude"].to_numpy())
+        lat_slice = slice(lat + radius * self.sp_res, lat - radius * self.sp_res)
+        lon_slice = slice(lon - radius * self.sp_res, lon + radius * self.sp_res)
 
-        lat_slice=slice(lat+radius*self.sp_res, lat-radius*self.sp_res)
-        lon_slice=slice(lon-radius*self.sp_res, lon+radius*self.sp_res)
+        time_slice = slice(time - weeks, time)
+        points_input_vars = (
+            self.cube[self.input_vars]
+            .sel(latitude=lat_slice, longitude=lon_slice)
+            .isel(time=time_slice)
+        )
+        points_target_vars = (
+            self.cube[self.input_vars]
+            .sel(latitude=lat_slice, longitude=lon_slice)
+            .isel(time=time_slice)
+        )
 
-        cur_point_input_vars = self.cube[self.input_vars].sel(latitude=lat_slice, longitude=lon_slice).isel(time=time)
-        cur_point_target_var = self.cube[self.target_var].sel(latitude=lat_slice, longitude=lon_slice).isel(time=time)
+        points_input_vars = points_input_vars.resample(time=time_aggregation).mean(
+            skipna=True
+        )
+        points_target_vars = points_target_vars.resample(time=time_aggregation).mean(
+            skipna=True
+        )
 
-        time_slice=slice(time-past_weeks, time)
-        past_points_input_vars = self.cube[self.input_vars].sel(latitude=lat_slice, longitude=lon_slice).isel(time=time_slice)
-        past_points_target_vars = self.cube[self.input_vars].sel(latitude=lat_slice, longitude=lon_slice).isel(time=time_slice)
+        time_dim = points_input_vars.time.shape[0]
+        latitude_dim = points_input_vars.latitude.shape[0]
+        longitude_dim = points_input_vars.longitude.shape[0]
+        logger.info(
+            "Sample dimensions: time={}, lat={}, lon={}".format(
+                time_dim, latitude_dim, longitude_dim
+            )
+        )
 
-        past_points_input_vars = past_points_input_vars.resample(time=time_aggregation).mean(skipna=True)
-        past_points_target_vars = past_points_target_vars.resample(time=time_aggregation).mean(skipna=True)
-        past_points_count = past_points_input_vars.time.shape[0]
+        time_coords = np.flip(points_input_vars.time.to_numpy())
+        logger.info("Coordinates in (reverse) time {}".format(time_coords))
 
-        # TODO: figure out a way to define by std and mean 
+        vertices_index = []
+        for time_index in time_coords: 
+            for lat_index in np.linspace(lat + radius * self.sp_res, lat - radius * self.sp_res, 2*radius+1):
+                for lon_index in np.linspace(lon - radius * self.sp_res, lon + radius * self.sp_res, 2*radius+1): 
+                    vertices_index.append((lat_index, lon_index,time_index))
+        print(vertices_index)
+
+        print("TODO")
+        vertices_input_vars = points_input_vars.stack(vertex=("latitude", "longitude", "time"))
+        vertices_target_vars = points_target_vars.stack(vertex=("latitude", "longitude", "time"))
+        vertices_dim = vertices_input_vars.vertex.shape[0]
+        print(vertices_input_vars.vertex)
+        logger.info("Graph will have {} vertices".format(vertices_dim))
+
+        #print(vertices_input_vars.vertex)
+        #print(vertices_input_vars)
+        #tmp  = vertices_input_vars.sel(vertex=(-23.875, 19.875, pd.Timestamp('2010-08-31 00:00:00')))
+        #print(tmp["ndvi"].to_numpy())
+        
+        #print(x)
+
+        # TODO: figure out a way to define by std and mean
 
         # TODO: build a graph
 
-        #data = self.cube.sel(
+        # data = self.cube.sel(
         #    latitude=lat, longitude=lon
-        #)#.isel(time=time)
-        #print(dataset)
-        #print(data["drought_code_max"].to_numpy())
-        
-        #print(region["area"])
+        # )#.isel(time=time)
+        # print(dataset)
+        # print(data["drought_code_max"].to_numpy())
+
+        # print(region["area"])
 
         pass
 
     def run(self):
-        #self.create_samples(
+        # self.create_samples(
         #    min_lon=20.375, min_lat=-24.375, max_lon=24.625, max_lat=-17.875
-        #)
-        self.create_sample(
-            lon=20.375, lat=-24.375, time=500
-        )
+        # )
+        self.create_sample(lat=-24.375, lon=20.375, time=500)
         pass
 
 
