b450a9c2d2a0346dac3d3204430cc9001dc3776e
Dimitrios Michail <dimitrios.michail@gmail.com>
2022-11-16 11:52:30 +0200

    More structure

--- create_dataset.py
+++ create_dataset.py
@@ -18,6 +18,8 @@ class DatasetBuilder:
         self,
         cube_path,
         output_folder,
+        split,
+        positive_samples_threshold
     ):
         self.cube_path = cube_path
         self.output_folder = output_folder
@@ -27,16 +29,25 @@ class DatasetBuilder:
             "tp",
             "drought_code_max",
             "sst",
+            "rel_hum",
             "lst_day",
         ]
         # one of gwis_ba, BurntArea, frpfire, co2fire, FCCI_BA, co2fire
         self.target_var = "gwis_ba"
 
+        if split not in ["train", "test", "val"]: 
+            raise ValueError("Wrong split type")
+        self.split = split
+
+        self.positive_samples_threshold = positive_samples_threshold
+
+        # split time periods
         self.time_train = (0, 874)  # 2001-2019
         self.time_test = (874, 920)  # 2020
         self.time_eval = (920, 966)  # 2021
 
         self.sp_res = 0.25  # Spatial resolution
+        self.mean_std_dict = {}
 
         for folder in [self.output_folder]:
             logger.info("Creating output folder {}".format(folder))
@@ -46,7 +57,11 @@ class DatasetBuilder:
         logger.info("Opening zarr file {}".format(self.cube_path))
         self.cube = xr.open_zarr(self.cube_path, consolidated=False)
         logger.info("Cube: {}".format(self.cube))
-        # print(self.cube.data_vars)
+        logger.info("Vars: {}".format(self.cube.data_vars))
+
+    def compute_mean_std_dict(): 
+        # compute mean_std_dict
+        pass
 
     def create_samples(self, min_lon, min_lat, max_lon, max_lat):
         logger.info("Creating samples")
@@ -104,10 +119,11 @@ class DatasetBuilder:
             for lat_index in np.linspace(lat + radius * self.sp_res, lat - radius * self.sp_res, 2*radius+1):
                 for lon_index in np.linspace(lon - radius * self.sp_res, lon + radius * self.sp_res, 2*radius+1): 
                     vertices_index.append((lat_index, lon_index,time_index))
-        print(vertices_index)
+        #print(vertices_index)
 
         print("TODO")
         vertices_input_vars = points_input_vars.stack(vertex=("latitude", "longitude", "time"))
+        print(vertices_input_vars)
         vertices_target_vars = points_target_vars.stack(vertex=("latitude", "longitude", "time"))
         vertices_dim = vertices_input_vars.vertex.shape[0]
         print(vertices_input_vars.vertex)
@@ -135,6 +151,16 @@ class DatasetBuilder:
         pass
 
     def run(self):
+        # Depending on split 
+
+        # compute mean and std
+
+        # Sample nodes above threshold
+
+        # call create sample
+
+        # write to folder
+
         # self.create_samples(
         #    min_lon=20.375, min_lat=-24.375, max_lon=24.625, max_lat=-17.875
         # )
@@ -148,6 +174,8 @@ def main(args):
     builder = DatasetBuilder(
         args.cube_path,
         args.output_folder,
+        args.split,
+        args.positive_samples_threshold
     )
     builder.run()
 
@@ -173,6 +201,24 @@ if __name__ == "__main__":
         default="dataset",
         help="Output folder",
     )
+    parser.add_argument(
+        "--split",
+        metavar="KEY",
+        type=str,
+        action="store",
+        dest="split",
+        default="train",
+        help="Split type",
+    )
+    parser.add_argument(
+        "--positive-samples-threshold",
+        metavar="KEY",
+        type=float,
+        action="store",
+        dest="positive_samples_threshold",
+        default=0.1,
+        help="Positive sample threshold",
+    )        
     parser.add_argument(
         "--todo",
         metavar="FLAG",
