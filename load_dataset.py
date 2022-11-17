79b9f53f85387dfcb027dc57b9e2d9ddf4e286de
EleannaPanagiotou <lefkipanagiotou@gmail.com>
2022-11-17 13:23:08 +0200

    load data class

---
+++ load_dataset.py
@@ -0,0 +1,33 @@
+import torch 
+from torch_geometric.data import Dataset
+import os
+
+
+class LoadData(Dataset):
+    def __init__(self, root_dir, transform=None): #, csv_file
+        '''
+        Desc
+        ----
+        Loads dataset of graphs. Each graph is a pt file.
+        Dataset consists of multiple pt files that are stored in the root_dir.
+
+        Args
+        ----
+        root_dir: str
+            * whole path where all graphs (pt files) stored.
+            
+        Returns
+        ----
+        All the torch_geometric.data.Data objects (graphs)
+        '''
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len([entry for entry in os.listdir(self.root_dir)])
+
+    def __getitem__(self, idx):
+        graph = torch.load(self.root_dir + f'/data_{idx}.pt')
+
+        return graph
+
