1b01d467025791ac4a60d349c486db1dd395158d
EleannaPanagiotou <lefkipanagiotou@gmail.com>
2022-11-20 22:51:27 +0200

    with standardization

--- load_dataset.py
+++ load_dataset.py
@@ -1,10 +1,11 @@
 import torch 
 from torch_geometric.data import Dataset
 import os
+from scale_dataset import *
 
 
 class LoadData(Dataset):
-    def __init__(self, root_dir, transform=None): #, csv_file
+    def __init__(self, root_dir, transforms): #, csv_file
         '''
         Desc
         ----
@@ -21,13 +22,21 @@ class LoadData(Dataset):
         All the torch_geometric.data.Data objects (graphs)
         '''
         self.root_dir = root_dir
-        self.transform = transform
+        self.transforms = transforms
 
     def __len__(self):
         return len([entry for entry in os.listdir(self.root_dir)])
 
     def __getitem__(self, idx):
         graph = torch.load(self.root_dir + f'/graph_{idx}.pt')
+        graph.x = torch.cat((graph.x[:,:4], graph.x[:,5:]), axis = 1)
 
+        number_of_nodes = graph.x.shape[0]
+
+        if self.transforms is not None:
+            graph.x[0] = self.transforms.transform(graph.x[0])
+            for node_idx in range(0, number_of_nodes):
+                graph.x[node_idx] = self.transforms.transform(graph.x[node_idx])
+        
         return graph
-
+    
