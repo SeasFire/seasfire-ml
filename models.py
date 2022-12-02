c2b219ddfb9b9f389b9ede67d6b346c3e202f11d
EleannaPanagiotou <lefkipanagiotou@gmail.com>
2022-12-02 22:45:11 +0200

    lstm and attention GCN not working yet

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric_temporal.nn.recurrent import A3TGCN
+
+
 import argparse
 import json
 import time
@@ -19,12 +24,13 @@ from torch_geometric.nn import GCNConv, GATConv, GATv2Conv, TransformerConv
 from torch_geometric.nn import global_mean_pool, global_max_pool
 from torch_geometric_temporal import GConvLSTM
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
 
 
 class GCN(torch.nn.Module):
-    def __init__(self, input_size, hidden_channels, learning_rate, weight_decay):
+    def __init__(self, input_size, hidden_channels, learning_rate, weight_decay, task):
         super(GCN, self).__init__()
+        self.task = task
 
         self.conv1 = GCNConv(input_size, hidden_channels)
 
@@ -33,7 +39,11 @@ class GCN(torch.nn.Module):
         self.conv3 = GCNConv(hidden_channels - 16, out_channels=hidden_channels - 16)
 
         self.lin1 = Linear(hidden_channels, hidden_channels)
-        self.lin2 = Linear(hidden_channels, 1)
+
+        if(self.task == 'regression' or ):
+            self.lin2 = Linear(hidden_channels, 1)
+        elif(self.task == 'binary'):
+            self.lin2 = Linear(hidden_channels, 1)
 
         self.optimizer = torch.optim.Adam(
             self.parameters(), lr=learning_rate, weight_decay=weight_decay
@@ -48,8 +58,6 @@ class GCN(torch.nn.Module):
         x = self.conv2(x, edge_index)
         x = x.relu()
         x = F.dropout(x, p=0.2, training=self.training)
-        # x = self.conv3(x, edge_index)
-        # x = x.relu()
 
         # Readout layer
         batch = torch.zeros(x.shape[0], dtype=int) if batch is None else batch
@@ -58,7 +66,6 @@ class GCN(torch.nn.Module):
         # x = global_max_pool(x, batch)
 
         # Final classifier
-
         x = self.lin1(x)
         x = x.relu()
         x = F.dropout(x, p=0.3, training=self.training)
@@ -66,3 +73,78 @@ class GCN(torch.nn.Module):
         x = x.relu()
 
         return x
+
+
+class AttentionGNN(torch.nn.Module):
+    def __init__(self, node_features, periods, learning_rate, weight_decay):
+        super(TemporalGNN, self).__init__()
+        # Attention Temporal Graph Convolutional Cell
+        self.tgnn = A3TGCN(in_channels=node_features, 
+                           out_channels=32, 
+                           periods=periods)
+        # Equals single-shot prediction
+        self.linear = torch.nn.Linear(32, 1)
+
+        self.optimizer = torch.optim.Adam(
+            self.parameters(), lr=learning_rate, weight_decay=weight_decay
+        )
+
+    def forward(self, x, edge_index):
+        """
+        x = Node features for T time steps
+        edge_index = Graph edge indices
+        """
+        h = self.tgnn(x, edge_index)
+        h = F.relu(h)
+        h = self.linear(h)
+        return h
+
+class LstmGCN(torch.nn.Module):
+    def __init__(self, node_features, hidden_channels, learning_rate, weight_decay, task):
+        super(RecurrentGCN, self).__init__()
+        # Documentation for GConvLSTM:
+        # https://pytorch-geometric-temporal.readthedocs.io/en/latest/modules/root.html#torch_geometric_temporal.nn.recurrent.gconv_lstm.GConvLSTM
+        self.task = task
+        
+        self.recurrent_1 = GConvLSTM(node_features, 16, K=12)
+        # self.recurrent_2 = GConvLSTM(32, 16, 4)
+
+        if(self.task == 'regression'):
+            self.lin1 = Linear(32, 1)
+        elif(self.task == 'binary'):
+            self.lin1 = Linear(16, 2)
+
+        self.optimizer = torch.optim.Adam(
+            self.parameters(), lr=learning_rate, weight_decay=weight_decay
+        )
+
+    def forward(self, x, edge_index, edge_weight=None):
+
+        print(x.shape)
+        print(edge_index.shape)
+
+        # Process the sequence of graphs with our 2 GConvLSTM layers
+        h = None
+        c = None
+
+        h, c = self.recurrent_1(x, edge_index, edge_weight, H=h, C=c) 
+        print("what")
+
+        # Feed hidden state output of first layer to the 2nd layer
+        # h2, c2 = self.recurrent_2(h1, edge_index, H=h2, C=c2)
+        # print("is")
+
+        # Use the final hidden state output of 2nd recurrent layer for input to classifier
+        x = F.relu(h1)
+        print("going")
+
+        x = F.dropout(x, training=self.training)
+        print("on")
+
+        x = self.lin1(x)
+        print("?????????????????????")
+
+        return x
+        # return F.log_softmax(x, dim=1)
+
+
