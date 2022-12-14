116e56274783dbaa6a4befa7cde201c8366606cb
EleannaPanagiotou <lefkipanagiotou@gmail.com>
2022-12-14 13:15:53 +0200

    pytorch accuracy added

--- train.py
+++ train.py
@@ -40,15 +40,18 @@ def train(model, train_loader, epochs, val_loader, batch_size, task):
     optimizer = model.optimizer
 
     criterion = 0
-    F1_score_test = 0
-    avprc_test = 0
-    auroc_test = 0
+    accuracy_test = 0
+    accuracy_val = 0
+    F1_score_val = 0
+    avprc_val = 0
+    auroc_val = 0
 
     if task == 'binary':
         criterion = torch.nn.CrossEntropyLoss()
-        F1_score_test = F1Score(task = 'binary', num_classes = 2).to(device)
-        avprc_test= AveragePrecision(task="multiclass", num_classes = 2).to(device)
-        auroc_test= AUROC(task="multiclass", num_classes = 2).to(device)
+        accuracy_val = Accuracy(task="multiclass", num_classes=2).to(device)
+        F1_score_val = F1Score(task = 'multiclass', num_classes = 2).to(device)
+        avprc_val= AveragePrecision(task="multiclass", num_classes = 2).to(device)
+        auroc_val= AUROC(task="multiclass", num_classes = 2).to(device)
     elif task == 'regression':
         criterion = torch.nn.MSELoss()
 
@@ -110,17 +113,18 @@ def train(model, train_loader, epochs, val_loader, batch_size, task):
                 val_labels.append(y)
 
                 if task == 'binary':
-                    F1_score_test.update(torch.argmax(torch.cat(val_predictions), dim=1), torch.argmax(torch.cat(val_labels), dim=1))
-                    avprc_test.update(torch.cat(val_predictions), torch.argmax(torch.cat(val_labels), dim=1))
-                    auroc_test.update(torch.cat(val_predictions), torch.argmax(torch.cat(val_labels), dim=1))
+                    accuracy_val.update(torch.argmax(torch.cat(val_predictions), dim=1), torch.argmax(torch.cat(val_labels), dim=1))
+                    F1_score_val.update(torch.argmax(torch.cat(val_predictions), dim=1), torch.argmax(torch.cat(val_labels), dim=1))
+                    avprc_val.update(torch.cat(val_predictions), torch.argmax(torch.cat(val_labels), dim=1))
+                    auroc_val.update(torch.cat(val_predictions), torch.argmax(torch.cat(val_labels), dim=1))
 
-                    argmax_pred = torch.argmax(preds, dim=1)
-                    # print("Argmax preds: ", argmax_pred)
+                    # argmax_pred = torch.argmax(preds, dim=1)
+                    # # print("Argmax preds: ", argmax_pred)
 
-                    argmax_y = torch.argmax(y, dim=1)
-                    # print("Argmax y: ", argmax_y)
+                    # argmax_y = torch.argmax(y, dim=1)
+                    # # print("Argmax y: ", argmax_y)
 
-                    val_results.append((argmax_pred == argmax_y))
+                    # val_results.append((argmax_pred == argmax_y))
             
         train_loss = criterion(torch.cat(train_labels), torch.cat(train_predictions))
         val_loss = criterion(torch.cat(val_labels), torch.cat(val_predictions))
@@ -129,23 +133,24 @@ def train(model, train_loader, epochs, val_loader, batch_size, task):
         # print(f"Epoch {epoch} | Train Loss: {train_loss}"  + f" | Val Loss: {val_loss}")
 
         if task == 'binary':
-            train_results = torch.cat(train_results)
-            train_accuracy = train_results.sum() / train_results.shape[0]
-            val_results = torch.cat(val_results)
-            val_accuracy = val_results.sum() / val_results.shape[0]
-            logger.info("| Train Accuracy: {:.4f}".format(train_accuracy))
-            logger.info("| Val Accuracy: {:.4f}".format(val_accuracy))
+            # train_results = torch.cat(train_results)
+            # train_accuracy = train_results.sum() / train_results.shape[0]
+            # val_results = torch.cat(val_results)
+            # val_accuracy = val_results.sum() / val_results.shape[0]
+            # logger.info("| Train Accuracy: {:.4f}".format(accuracy_train.compute()))
+            logger.info("| Val Accuracy: {:.4f}".format(accuracy_val.compute()))
 
-            logger.info("| Val F1_score: {:.4f}".format(F1_score_test.compute()))
-            logger.info("| Val Average precision: {:.4f}".format(avprc_test.compute()))
-            logger.info("| AUROC: {:.4f}".format(auroc_test.compute()))
+            logger.info("| Val F1_score: {:.4f}".format(F1_score_val.compute()))
+            logger.info("| Val Average precision: {:.4f}".format(avprc_val.compute()))
+            logger.info("| AUROC: {:.4f}".format(auroc_val.compute()))
             
-            if avprc_test.compute()>current_max_avg:
+            if avprc_val.compute()>current_max_avg:
                 best_model = model
 
-            F1_score_test.reset()
-            avprc_test.reset()
-            auroc_test.reset()
+            accuracy_val.reset()
+            F1_score_val.reset()
+            avprc_val.reset()
+            auroc_val.reset()
         
 #    #     # print(train_labels)
     #     # print(train_predictions)
@@ -206,11 +211,20 @@ def main(args):
     )
 
     logger.info("Building model {}".format(args.model_name))
+
+    if args.task == 'binary':
+        linear_out_channels = 2
+        args.hidden_channels = args.hidden_channels + (linear_out_channels,)
+    elif args.task == 'regression':
+        linear_out_channels = 1
+        args.hidden_channels = args.hidden_channels + (linear_out_channels,)
+
     if args.model_name == "AttentionGNN":
-        num_node_features = 12
+        num_features = train_dataset.num_node_features
+        print(num_features)
         timesteps = args.timesteps
         model = AttentionGNN(
-            num_node_features, timesteps, args.learning_rate, args.weight_decay
+            num_node_features, args.hidden_channels, timesteps, args.learning_rate, args.weight_decay
         ).to(device)
     else:
         raise ValueError("Invalid model")
@@ -304,14 +318,13 @@ if __name__ == "__main__":
         help="Batch size",
     )
     parser.add_argument(
-        "-ch",
         "--hidden-channels",
         metavar="KEY",
-        type=int,
+        type=tuple,
         action="store",
         dest="hidden_channels",
-        default=64,
-        help="Hidden channels",
+        default=(32,16),
+        help="Hidden channels for layer 1 and layer 2 of GCN",
     )
     parser.add_argument(
         "-e",
