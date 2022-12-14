aed15d7b3285b89cc547515fd106a8bf056685db
EleannaPanagiotou <lefkipanagiotou@gmail.com>
2022-12-14 09:33:17 +0200

    minor

--- models.py
+++ models.py
@@ -21,14 +21,14 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class AttentionGNN(torch.nn.Module):
     def __init__(self, node_features, periods, learning_rate, weight_decay):
         super(AttentionGNN, self).__init__()
+        
         # Attention Temporal Graph Convolutional Cell
         self.tgnn = my_A3TGCN(in_channels=node_features, 
                            out_channels=32, 
                            periods=periods)
-        # Equals single-shot prediction
+    
         self.linear = torch.nn.Linear(16, 2)
 
-        # print(self.parameters)
         self.optimizer = torch.optim.Adam(
             self.parameters(), lr=learning_rate, weight_decay=weight_decay
         )
@@ -38,14 +38,14 @@ class AttentionGNN(torch.nn.Module):
         x = Node features for T time steps
         edge_index = Graph edge indices
         """
-
         h = self.tgnn(x, edge_index, readout_batch)
         h.to(device)
         h = F.relu(h)
 
         h = self.linear(h)
+        
         if task == 'binary':
             h = torch.softmax(h, dim=1)
-            
+
         return h
 
