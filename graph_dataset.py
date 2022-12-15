f810c8f7b3a1a47ff3a88823fe9087f3b69b90fa
Dimitrios Michail <dimitrios.michail@gmail.com>
2022-12-15 07:50:26 +0200

    Reverted graph dataset to not keep graph in memory

--- graph_dataset.py
+++ graph_dataset.py
@@ -29,30 +29,32 @@ class GraphDataset(Dataset):
 
     def __getitem__(self, idx):
         graph = torch.load(os.path.join(self.root_dir, "graph_{}.pt".format(idx)))
-        self.graph = graph
         
         # Define label
         if self.task == "binary":
-            self.graph.y = torch.where(self.graph.y > 0.0, 1, 0)
-            self.graph.y = torch.nn.functional.one_hot(self.graph.y, 2).float()
+            graph.y = torch.where(graph.y > 0.0, 1, 0)
+            graph.y = torch.nn.functional.one_hot(graph.y, 2).float()
         elif self.task == "regression":
-            self.graph.y = self.graph.y / 1000.0
+            graph.y = graph.y / 1000.0
         else:
             raise ValueError("Invalid task")
 
         # Standardize features
         if self.transform is not None:
-            self.graph.x = self.transform.transform(self.graph.x)
-            self.graph.x = torch.nan_to_num(self.graph.x, nan=-1.0)
+            graph.x = self.transform.transform(graph.x)
+            graph.x = torch.nan_to_num(graph.x, nan=-1.0)
 
         # Concatenate positions with features
         if self.append_position_as_feature:
-            positions = self.graph.pos.unsqueeze(2).expand(-1, -1, self.graph.x.shape[2])
-            self.graph.x = torch.cat((self.graph.x, positions), dim=1)
+            positions = graph.pos.unsqueeze(2).expand(-1, -1, graph.x.shape[2])
+            graph.x = torch.cat((graph.x, positions), dim=1)
 
-        return self.graph
+        return graph
 
     def num_features(self) -> int:
         r"""Returns the number of features per node in the dataset.
         Alias for :py:attr:`~num_node_features`."""
-        return self.graph.x.shape[1]
+        graph = torch.load(os.path.join(self.root_dir, "graph_{}.pt".format(0)))
+        if self.append_position_as_feature: 
+            return graph.x.shape[1] + graph.pos.shape[1]
+        return graph.x.shape[1]
