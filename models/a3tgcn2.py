036050a9cef4805656e9731dfd8a7612af8771bf
EleannaPanagiotou <lefkipanagiotou@gmail.com>
2022-12-16 09:42:02 +0200

    as in main

--- models/a3tgcn2.py
+++ models/a3tgcn2.py
@@ -7,7 +7,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class A3TGCN2(torch.nn.Module):
     r"""A version of A3T-GCN with multiple layers.`_
-
     Args:
         in_channels (int): Number of input features.
         out_channels (int): Number of output features.
@@ -54,18 +53,17 @@ class A3TGCN2(torch.nn.Module):
         edge_index: torch.LongTensor,
         edge_weight: torch.FloatTensor = None,
         H: torch.FloatTensor = None,
+        readout_batch=None,
     ) -> torch.FloatTensor:
         """
         Making a forward pass. If edge weights are not present the forward pass
         defaults to an unweighted graph. If the hidden state matrix is not present
         when the forward pass is called it is initialized with zeros.
-
         Arg types:
             * **X** (PyTorch Float Tensor): Node features for T time periods.
             * **edge_index** (PyTorch Long Tensor): Graph edge indices.
             * **edge_weight** (PyTorch Long Tensor, optional)*: Edge weight vector.
             * **H** (PyTorch Float Tensor, optional): Hidden state matrix for all nodes.
-
         Return types:
             * **H** (PyTorch Float Tensor): Hidden state matrix for all nodes.
         """
@@ -73,16 +71,20 @@ class A3TGCN2(torch.nn.Module):
         probs = torch.nn.functional.softmax(self.attention, dim=0)
         for period in range(self.periods):
             H_accum = H_accum + probs[period] * self._base_tgcn(
-                X[:, :, period], edge_index, edge_weight, H
+                X[:, :, period], edge_index, edge_weight, H, readout_batch
             )
         return H_accum
 
 
 class AttentionGNN(torch.nn.Module):
-    def __init__(self, node_features, output_channels, periods, learning_rate, weight_decay):
+    def __init__(
+        self, node_features, output_channels, periods, learning_rate, weight_decay, task
+    ):
         super(AttentionGNN, self).__init__()
         # Attention Temporal Graph Convolutional Cell with 2 layers
-        self.tgnn = A3TGCN2(in_channels=node_features, out_channels=output_channels, periods=periods)
+        self.tgnn = A3TGCN2(
+            in_channels=node_features, out_channels=output_channels, periods=periods
+        )
         # Equals single-shot prediction
         self.linear = torch.nn.Linear(output_channels[1], output_channels[2])
 
@@ -91,18 +93,27 @@ class AttentionGNN(torch.nn.Module):
             self.parameters(), lr=learning_rate, weight_decay=weight_decay
         )
 
-    def forward(self, x, edge_index, task, readout_batch=None):
+        self.task = task
+
+    def forward(
+        self,
+        X: torch.FloatTensor,
+        edge_index: torch.LongTensor,
+        edge_weight: torch.FloatTensor = None,
+        H: torch.FloatTensor = None,
+        readout_batch=None,
+    ) -> torch.FloatTensor:
         """
         x = Node features for T time steps
         edge_index = Graph edge indices
         """
 
-        h = self.tgnn(x, edge_index, readout_batch)
+        h = self.tgnn(X, edge_index, edge_weight, H, readout_batch)
         h.to(device)
         h = F.relu(h)
 
         h = self.linear(h)
-        if task == "binary":
+        if self.task == "binary":
             h = torch.softmax(h, dim=1)
 
         return h
