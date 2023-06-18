ff2b557e1111cc947ebba0785b957734bd6a29cc
Dimitrios Michail <dimitrios.michail@gmail.com>
2023-06-18 13:55:09 +0300

    Fixed bug

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from transforms import GraphNormalize, ToCentralNodeAndNormalize
-from mean_std import compute_mean_std_per_feature
-from graph_dataset import GraphDataset
+from .transforms import GraphNormalize, ToCentralNodeAndNormalize
+from .mean_std import compute_mean_std_per_feature
+from .graph_dataset import GraphDataset
