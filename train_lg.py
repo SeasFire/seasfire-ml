4d5e1d8aa64a8aab0db84787e9c5bae05567baac
Dimitrios Michail <dimitrios.michail@gmail.com>
2023-07-19 08:49:35 +0300

    Refactored checkpoint methods

--- train_lg.py
+++ train_lg.py
@@ -18,6 +18,8 @@ from models import (
 from utils import (
     LocalGlobalDataset,
     LocalGlobalTransform,
+    load_checkpoint, 
+    save_checkpoint,
 )
 
 
@@ -248,38 +250,6 @@ def save_model(model, model_type, model_name, out_dir):
     logger.info("Saving model as {}".format(filename))
     torch.save(model.state_dict(),filename)
 
-def save_checkpoint(model, epoch, best_so_far, optimizer, scheduler, model_name, out_dir):
-    filename = "{}/{}.checkpoint.pt".format(out_dir, model_name)
-    logger.info("Saving checkpoint as {}".format(filename))
-
-    torch.save(model.state_dict(), "{}/{}.checkpoint.model.pt".format(out_dir, model_name))
-    torch.save(optimizer.state_dict(), "{}/{}.checkpoint.optimizer.pt".format(out_dir, model_name))
-    torch.save(scheduler.state_dict(), "{}/{}.checkpoint.scheduler.pt".format(out_dir, model_name))
-    torch.save(epoch, "{}/{}.checkpoint.epoch.pt".format(out_dir, model_name))
-    torch.save(best_so_far, "{}/{}.checkpoint.best_so_far.pt".format(out_dir, model_name))
-
-def load_checkpoint(model_name, out_dir):
-    epoch = 0
-    if os.path.exists("{}/{}.checkpoint.epoch.pt".format(out_dir, model_name)): 
-        epoch = torch.load("{}/{}.checkpoint.epoch.pt".format(out_dir, model_name))
-
-    model_state_dict = None
-    if os.path.exists("{}/{}.checkpoint.model.pt".format(out_dir, model_name)): 
-        model_state_dict = torch.load("{}/{}.checkpoint.model.pt".format(out_dir, model_name), map_location=device)
-
-    optimizer_state_dict = None
-    if os.path.exists("{}/{}.checkpoint.optimizer.pt".format(out_dir, model_name)): 
-        optimizer_state_dict = torch.load("{}/{}.checkpoint.optimizer.pt".format(out_dir, model_name), map_location=device)
-
-    scheduler_state_dict = None
-    if os.path.exists("{}/{}.checkpoint.scheduler.pt".format(out_dir, model_name)): 
-        scheduler_state_dict = torch.load("{}/{}.checkpoint.scheduler.pt".format(out_dir, model_name), map_location=device)
-
-    best_so_far = None
-    if os.path.exists("{}/{}.checkpoint.best_so_far.pt".format(out_dir, model_name)): 
-        best_so_far = torch.load("{}/{}.checkpoint.best_so_far.pt".format(out_dir, model_name))    
-
-    return (model_state_dict, optimizer_state_dict, scheduler_state_dict, epoch, best_so_far)
 
 
 def main(args):
@@ -381,7 +351,7 @@ def main(args):
         args.hidden_channels,
         args.global_timesteps,
         train_dataset.global_nodes,
-        args.decoder_hidden_channels if not args.include_global else None,
+        args.decoder_hidden_channels,
         args.include_global,
     )
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .local_global_dataset import LocalGlobalDataset, LocalGlobalTransform
 from .gru_dataset import GRUDataset, GRUTransform
+from .checkpoint import load_checkpoint, save_checkpoint
