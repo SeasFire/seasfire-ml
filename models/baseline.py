Fixed baseline computation
 - Remove threshold in code
 - Used sklearn instead of torch

models/baseline.py
@@ -67,6 +67,7 @@ class BaselineModel:
             )
             .fillna(0)
         )
+
         previous = target[time_idx-self._year_in_weeks::-self._year_in_weeks]
 
         if self._method == "mean":
@@ -74,10 +75,9 @@ class BaselineModel:
             logger.debug("mean={}".format(mean.values))
             return torch.as_tensor(mean.values)
         elif self._method == "majority":
-            zero_count = (previous == 0).sum().values
             non_zero_count = (previous != 0).sum().values
-            majority = 1.0 if non_zero_count >= zero_count else 0.0
-            logger.debug("majority={} since {} (non-zeros) >= {} (zeros)".format(majority, non_zero_count, zero_count))
+            majority = non_zero_count / previous.size
+            logger.debug("majority={} since {} (non-zeros) in total {}".format(majority, non_zero_count, previous.size))
             return torch.as_tensor(majority)
         else: 
             raise ValueError("Invalid method")

test_baseline.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 import os
 import torch
 import torch_geometric
-from torchmetrics import AUROC, Accuracy, AveragePrecision, F1Score, StatScores, Recall
+from sklearn.metrics import average_precision_score
 from utils import (
     LocalGlobalDataset,
     LocalGlobalTransform
@@ -22,37 +22,28 @@ def test(model, loader, model_name):
     logger.info("Starting Test")
 
     with torch.no_grad():
-        metrics = [
-            ("Accuracy", Accuracy(task="binary").to(device)),
-            ("Recall", Recall(task="binary").to(device)),
-            ("F1Score", F1Score(task="binary").to(device)),
-            ("AveragePrecision (AUPRC)", AveragePrecision(task="binary").to(device)),
-            ("AUROC", AUROC(task="binary").to(device)),
-            ("StatScores", StatScores(task="binary").to(device))
-        ]
+        all_y_true = []
+        all_y_score = []
 
         for _, data in enumerate(tqdm(loader)):
             lat = data.center_lat
             lon = data.center_lon
             time = data.center_time
 
-            y = data.y
-            preds = model((lat, lon, time))
+            y_true = data.y
+            y_score = model((lat, lon, time))
 
-            preds = preds.gt(0.0).float()
-            y = y.gt(0.0)
-
-            for _, metric in metrics:
-                metric.update(preds, y)
-
-        result = "{}".format(model_name)
-        for metric_name, metric in metrics:
-            metric_value = metric.compute()
-            logger.info("| Test {}: {}".format(metric_name, metric_value))
-            result += ",{}".format(metric_value)
-            metric.reset()
-
-        logger.info(result)
+            y_true = y_true.gt(0.0).detach().cpu().numpy()
+            y_true = 1*y_true
+            logger.debug("y_true={}".format(y_true))
+            all_y_true.extend(y_true)
+            
+            y_score = y_score.detach().cpu().numpy()
+            logger.debug("y_score={}".format(y_score))
+            all_y_score.extend(y_score)
+ 
+        avg_precision = average_precision_score(all_y_true, all_y_score)
+        logger.info("{} Test AveragePrecision (AUPRC): {}".format(model_name, avg_precision))
 
 
 def main(args):